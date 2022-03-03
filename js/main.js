1c26c21eec299eb46acde7965758d8f974d53d72
VESHENA WILLIAMS <98852629+veshena@users.noreply.github.com>
2022-03-02 20:01:49 -0500

    Add restart n lives count

--- js/main.js
+++ js/main.js
@@ -1,21 +1,21 @@
 // Get the section and span from the body
 const section = document.querySelector("section");
 const livesCount = document.querySelector("span");
-const lives = 3;
+let lives = 3;
 
 // Link text
 livesCount.textContent = lives;
 
 // Generate Object
 const getData = () => [
-    {imgSrc: "./images/cashew.jpeg", name: "cashew"}, 
-    {imgSrc: "./images/mango.jpeg", name: "mango"},
-    {imgSrc: "./images/plum.jpeg", name: "plum"},
-    {imgSrc: "./images/pomerac.jpeg", name: "pomerac"},
-    {imgSrc: "./images/cashew.jpeg", name: "cashew"},
-    {imgSrc: "./images/mango.jpeg", name: "mango"},
-    {imgSrc: "./images/plum.jpeg", name: "plum"},
-    {imgSrc: "./images/pomerac.jpeg", name: "pomerac"},
+    { imgSrc: "./images/cashew.jpeg", name: "cashew" },
+    { imgSrc: "./images/mango.jpeg", name: "mango" },
+    { imgSrc: "./images/plum.jpeg", name: "plum" },
+    { imgSrc: "./images/pomerac.jpeg", name: "pomerac" },
+    { imgSrc: "./images/cashew.jpeg", name: "cashew" },
+    { imgSrc: "./images/mango.jpeg", name: "mango" },
+    { imgSrc: "./images/plum.jpeg", name: "plum" },
+    { imgSrc: "./images/pomerac.jpeg", name: "pomerac" },
 ];
 
 // Randomize
@@ -30,54 +30,75 @@ const randomize = () => {
 const cardGenerator = () => {
     const cardData = randomize();
     // console.log(cardData)
-    
-// Generate HTML
-   cardData.forEach(item => {
-    // console.log(item);
-    
-    const card = document.createElement("div");
-    const face = document.createElement("img");
-    const back = document.createElement("div");
-    card.classList = "card";
-    face.classList = "face";
-    back.classList = "back";
-    
-    // Attach info to cards
-    face.src = item.imgSrc;
-card.setAttribute("name", item.name)
 
-    
-    // Attach cards to section
-    section.appendChild(card);
-    card.appendChild(face);
-    card.appendChild(back);
+    // Generate HTML
+    cardData.forEach(item => {
+        // console.log(item);
 
-    card.addEventListener("click", (v) => {
-        card.classList.toggle("toggleCard");
-        checkCards(v);
+        const card = document.createElement("div");
+        const face = document.createElement("img");
+        const back = document.createElement("div");
+        card.classList = "card";
+        face.classList = "face";
+        back.classList = "back";
+
+        // Attach info to cards
+        face.src = item.imgSrc;
+        card.setAttribute("name", item.name)
+
+
+        // Attach cards to section
+        section.appendChild(card);
+        card.appendChild(face);
+        card.appendChild(back);
+
+        card.addEventListener("click", (v) => {
+            card.classList.toggle("toggleCard");
+            checkCards(v);
+        });
     });
-});
 };
 // Check cards
 const checkCards = (v) => {
     console.log(v);
-    
+
     const clickedCard = v.target;
     clickedCard.classList.add("flipped");
     const flippedCards = document.querySelectorAll(".flipped");
     console.log(flippedCards);
-    
+
     // if statement
-if(flippedCards.length === 2){
-    if(
-        flippedCards[0].getAttribute("name") === flippedCards[1].getAttribute("name")
-    ) {
-        console.log("match");
-    } else {
-        console.log("wrong")
+    if (flippedCards.length === 2) {
+        if (
+            flippedCards[0].getAttribute("name") === flippedCards[1].getAttribute("name")
+        ) {
+            console.log("match");
+            flippedCards.forEach(card => {
+                card.classList.remove("flipped")
+                card.style.pointerEvent = "none";
+            })
+        } else {
+            console.log("wrong")
+            flippedCards.forEach(card => {
+                card.classList.remove("flipped");
+                setTimeout(() => card.classList.remove("toggleCard"), 1000);
+            });
+            lives--;
+            livesCount.textContent = lives;
+            if(lives === 0) {
+                restart();
+            }
+        }
     }
-}
 };
 
-
+// Restart
+const restart = () => {
+    let cardData = randomize();
+    let faces = document.querySelectorAll(".face");
+    let cards = document.querySelectorAll(".card");
+    cardData.forEach((item,index) => {
+        cards[index].classList.remove("toggleCard");
+    })
+}
 cardGenerator();
